9dfc24fb49863ed98e2ad1b7b2a6bd13168008f8
minsushin-kor <minsushin425@gmail.com>
2026-04-21 12:14:19 +0900

    filter

---
+++ src/main/java/com/winter/app/config/FilterConfig.java
@@ -0,0 +1,28 @@
+package com.winter.app.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.winter.app.filter.TestFilter;
+
+import jakarta.servlet.Filter;
+
+@Configuration
+public class FilterConfig implements WebMvcConfigurer{
+	
+	//@Bean
+	FilterRegistrationBean<Filter> filterRegistrationBean(){
+		
+		FilterRegistrationBean<Filter> f = new FilterRegistrationBean<>();
+		
+		f.setFilter(new TestFilter());
+		f.addUrlPatterns("/qna/detail", "/qna/create", "/qna/update", "/qna/delete");
+		f.addUrlPatterns("/member/mypage", "/member/logout");
+		
+		return f;
+		
+	}
+	
+}
